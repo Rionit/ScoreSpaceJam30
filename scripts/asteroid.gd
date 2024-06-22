b3fd7bbadaebc11a29b9e894bcc43bde9c617025
dolezfil <dolezfil@fel.cvut.cz>
2024-06-22 23:53:26 +0200

    Weeeeeeeee

--- scripts/asteroid.gd
+++ scripts/asteroid.gd
@@ -1,15 +1,15 @@
 extends RigidBody2D
 
+const _f = 50
+const _a = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	randomize()
+	var loc = Vector2(randf_range(-_f, _f), randf_range(-_f, _f))
+	var offset = Vector2(randf_range(-_f, _f), randf_range(-_f, _f))
+	apply_impulse(loc, offset)
+	angular_velocity = randf_range(-_a, _a)
 
 func destroy():
 	queue_free()
